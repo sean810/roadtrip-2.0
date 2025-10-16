316714494cbbd2c58deee2fafc05ce16c4bd72e4
sean810 <seantashel@gmail.com>
2025-10-16 17:17:34 +0300

    Added constants.js for storing reusable values like APi_endpoints etc

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+export const SPACING = {
+  xs: "8px",
+  sm: "12px",
+  md: "16px",
+  lg: "20px",
+  xl: "24px",
+  "2xl": "32px"
+};
+
+export const BREAKPOINTS = {
+  mobile: "480px",
+  tablet: "768px",
+  desktop: "1024px",
+  wide: "1280px"
+};
+
+export const TRANSITIONS = {
+  fast: "150ms",
+  base: "250ms",
+  slow: "360ms"
+};
+
+export const API_ENDPOINTS = {
+  weather: "https://api.openweathermap.org/data/2.5/weather",
+  maps: "https://www.google.com/maps"
+};
